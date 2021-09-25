57a7eabfdf8b75aff901df3237829977ed21817d
Sanket Deotale <sanket.dnyaneshwar2019@vitbhopal.ac.in>
2021-09-25 21:13:14 +0530

    Create Demo2.java

---
+++ Basics/Demo2.java
@@ -0,0 +1,15 @@
+public class Demo2 {
+   static int age=20;
+   String name="abc";
+   void show() {
+        System.out.println(age);
+        System.out.println(name);
+   }
+   public static void main(String[] args){
+         Demo2 obj=new Demo2();
+        obj.show();
+   
+       
+    }
+    
+}
