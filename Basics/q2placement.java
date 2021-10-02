88f4d94afb7e87e4e1cd86d63b79cba309a92914
Sanket Deotale <sanket.dnyaneshwar2019@vitbhopal.ac.in>
2021-10-02 12:38:52 +0530

    Create q2placement.java

---
+++ Basics/q2placement.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+public class q2placement
+{
+    public static void main(String[]args)
+{
+    Scanner sc=new Scanner(System.in);
+    char a=sc.next().charAt(0);
+    switch(a)
+    {
+        case'a':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'A':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'e':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'E':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'i':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'I':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'o':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'O':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'u':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        case'U':
+        {
+        System.out.println("It is a vowel.");
+        break;
+        }
+        default:
+        {
+        System.out.println("It is a consonent.");
+        }
+    }
+    sc.close();
+}
+}
